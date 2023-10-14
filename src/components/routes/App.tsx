191a97a51940e18db0ba3ac692295af26bf7fcb3
Kamila Marcinek <kamila.weronika.marcinek@gmail.com>
2023-10-14 15:19:33 +0200

    folder structure

---
+++ src/components/routes/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const App: FC = () =>{
+  return (
+    <>tutaj będzie strona "logowania" gdzie trzeba będzie wpisać istniejący adres email, lub też będzie formularz rejestracji dynamicznie przęłączany w obrębie app. </>
+  );
+}
+
+export default App;
