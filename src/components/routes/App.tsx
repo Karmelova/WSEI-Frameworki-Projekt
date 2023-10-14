4a709cb17663f9f29643a4e514dfd0cab96cd21d
Kamila Marcinek <kamila.weronika.marcinek@gmail.com>
2023-10-14 15:27:37 +0200

    added description what app i want to do

--- src/components/routes/App.tsx
+++ src/components/routes/App.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const App: FC = () =>{
   return (
-    <>tutaj będzie strona "logowania" gdzie trzeba będzie wpisać istniejący adres email, lub też będzie formularz rejestracji dynamicznie przęłączany w obrębie app. </>
+    <>tutaj będzie strona "logowania" gdzie trzeba będzie wpisać istniejący adres email, lub też będzie formularz rejestracji dynamicznie przęłączany w obrębie app. po "zalogowaniu" użytkownikowi wyświetli się blog z postami od różnych użytkowników. Bardzo minimalistyczna kopia facebooka/pinterest "Your-story"</>
   );
 }
 
