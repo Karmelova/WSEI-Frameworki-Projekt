8d874509f98ef29fb89bb49d2d56f2323cf157c6
Kamila Marcinek <kamila.weronika.marcinek@gmail.com>
2023-10-28 15:57:48 +0200

    routes added

--- src/components/routes/App.tsx
+++ src/components/routes/App.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
+import {Routes, Route} from 'react-router-dom'
+// import Profile from 'Profile';
 
 export const App: FC = () =>{
   return (
-    <>tutaj będzie strona "logowania" gdzie trzeba będzie wpisać istniejący adres email, lub też będzie formularz rejestracji dynamicznie przęłączany w obrębie app. po "zalogowaniu" użytkownikowi wyświetli się blog z postami od różnych użytkowników. Bardzo minimalistyczna kopia facebooka/pinterest "Your-story"</>
+    <Routes>
+      <>tutaj będzie strona "logowania" gdzie trzeba będzie wpisać istniejący adres email, lub też będzie formularz rejestracji dynamicznie przęłączany w obrębie app. po "zalogowaniu" użytkownikowi wyświetli się blog z postami od różnych użytkowników. Bardzo minimalistyczna kopia facebooka/pinterest "Your-story"</>
+      {/* <Route path="/" element={<Profile />}></Route> */}
+    </Routes>
+    
   );
 }
 
