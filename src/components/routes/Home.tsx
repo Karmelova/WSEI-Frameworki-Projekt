323b62846068f0de074cf1f94fe421ab104eb721
Karmelova <kamila.weronika.marcinek@gmail.com>
2024-01-03 22:53:23 +0100

    home - info

--- src/components/routes/Home.tsx
+++ src/components/routes/Home.tsx
@@ -1,4 +1,4 @@
 export const Home = () =>{
-    return (<></>);
+    return (<div> <p>tutaj będzie strona główna, ale jest narazie pusto :D</p> <p>zobacz <a style={{fontFamily:"sans-serif"}} href="/posts">posty</a>, <a style={{fontFamily:"sans-serif"}} href="/albums">albumy</a> i <a style={{fontFamily:"sans-serif"}} href="/users">użytkowników</a></p></div>);
 }
 export default Home;
